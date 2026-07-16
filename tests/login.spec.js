cc0ca1169dc0ced2d64abee29e6dd02f1a4fc145
unknown <mustafa.ata@anzera.com.tr>
2026-07-17 00:45:05 +0300

    feat: create first Playwright login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,12 @@
+
 import { test, expect } from '@playwright/test';
-
-test('User can login with valid credentials', async ({ page }) => {
+test('User can login with valid credentials', async ({page}) => {
   await page.goto('https://www.saucedemo.com/');
+  await page.locator('#user-name').fill('standard_user');
+  await page.locator('#password').fill('secret_sauce');
+ await page.locator('#login-button').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
-  await page.fill('#user-name', 'standard_user');
-  await page.fill('#password', 'secret_sauce');
 
-  await page.click('#login-button');
 
-  await expect(page).toHaveURL(/inventory/);
 });
