a69921e2eb52de19b91cb6055d90a5f69d23f2d9
unknown <mustafa.ata@anzera.com.tr>
2026-07-18 15:13:17 +0300

    feat: add Playwright assertions and locator examples

--- tests/locator.spec.js
+++ tests/locator.spec.js
@@ -1,17 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('Locator examples', async ({ page }) => {
+test('Button should be enabled', async ({ page }) => {
+
   await page.goto('https://www.saucedemo.com/');
 
-  // ID ile
-  await page.locator('#user-name').fill('standard_user');
+  await expect(
+    page.locator('[data-test="login-button"]')
+  ).toBeEnabled();
 
-  // data-test ile
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-
-  // Role ile
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  // URL doğrulama
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,15 +1,46 @@
-
 import { test, expect } from '@playwright/test';
-test('User can login with valid credentials', async ({page}) => {
+
+test('User can login with valid credentials', async ({ page }) => {
+
   await page.goto('https://www.saucedemo.com/');
+
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
+
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
 await expect(
-        page.locator('.title')
-    ).toHaveText('Products');
+    page.locator('.title')
+).toBeVisible();
 
-    
+await expect(
+    page.locator('.title')
+).toHaveText('Products');
+
+await expect(page.locator('.title')).toContainText('Prod');
+await expect(
+    page.locator('.inventory_item')
+).toHaveCount(6);
+
+const firstProduct = page.locator('.inventory_item').first();
+
+await expect(
+    firstProduct.locator('.inventory_item_name')
+).toHaveText('Sauce Labs Backpack');
+
+const lastProduct = page.locator('.inventory_item').last()
+
+await expect(
+lastProduct.locator('.inventory_item_name')
+).toHaveText('Test.allTheThings() T-Shirt (Red)');
+
+const secondProduct = page.locator('.inventory_item').nth(1)
+await expect (
+
+  secondProduct.locator('.inventory_item_name')
+).toHaveText('Sauce Labs Bike Light');
+
+
+
 });
