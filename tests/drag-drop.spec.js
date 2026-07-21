407a1faab63fabdc9fcd4307e4ff61b0558b2373
unknown <mustafa.ata@anzera.com.tr>
2026-07-21 20:06:46 +0300

    feat: add drag and drop automation test

---
+++ tests/drag-drop.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Drag and Drop', async ({ page }) => {
+
+    await page.goto('https://the-internet.herokuapp.com/drag_and_drop');
+
+    const columnA = page.locator('#column-a');
+    const columnB = page.locator('#column-b');
+
+    await columnA.dragTo(columnB);
+
+    await expect(columnA).toHaveText('B');
+
+    await expect(columnB).toHaveText('A');
+
+});
